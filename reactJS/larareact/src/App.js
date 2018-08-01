3da0fa039ff2ad15d014750df839a21dd9d41036
lal <eduardo.rasgado@hotmail.com>
2018-07-31 21:28:42 -0500

    hadle the load of data

--- reactJS/larareact/src/App.js
+++ reactJS/larareact/src/App.js
@@ -6,29 +6,39 @@ class App extends Component {
     super(props)
     // state for data local storage
     this.state = {
-      users: []
+      users: [],
+      loading: false
     }
+  }
+
+  getUsers() {
+    this.setState({
+      loading: true
+    })
+    //Call to a API using axios
+    axios('https://api.randomuser.me/?nat=US&results=5')
+    .then((response) => this.setState({
+      users: response.data.results,
+      loading: false
+    }))
   }
 
   //just before the component mounts
   componentWillMount() {
-    //Call to a API using axios
-    axios('https://api.randomuser.me/?nat=US&results=5')
-    .then((response) => this.setState({
-      users: response.data.results
-    }))
+    this.getUsers()
   }
   render() {
     return (
      <div className="App">
-      { this.state.users.map((user) => (
+      { !this.state.loading ? 
+        this.state.users.map((user) => (
         <div key={user.id.value}>
           <h3>{user.name.first.split("").map((l, key) => key === 0 ? l.toUpperCase() : l)}</h3>
           Email is { user.email }
           <hr/>
         </div>
       )) 
-      }
+      : 'Loading'}
      </div>
     );
   }
