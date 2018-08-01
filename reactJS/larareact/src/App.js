17b70c3eb22c2adec1eeb52e2bcbb8e4165958bf
lal <eduardo.rasgado@hotmail.com>
2018-07-31 21:08:44 -0500

    axios to extract data from api

--- reactJS/larareact/src/App.js
+++ reactJS/larareact/src/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    // state for data local storage
+    this.state = {
+      users: []
+    }
+  }
+
+  //just before the component mounts
+  componentWillMount() {
+    //Call to a API using axios
+    axios('https://api.randomuser.me/?nat=US&results=5')
+    .then((response) => this.setState({
+      users: response.data.results
+    }))
+  }
   render() {
     return (
      <div className="App">
-      We will be back!
+      { this.state.users.map((user) => (
+        <div key={user.id.value}>
+          <h3>{user.name.first.split("").map((l, key) => key === 0 ? l.toUpperCase() : l)}</h3>
+          Email is { user.email }
+          <hr/>
+        </div>
+      )) 
+      }
      </div>
     );
   }
